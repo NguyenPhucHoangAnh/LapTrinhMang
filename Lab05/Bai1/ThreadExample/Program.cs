fcecf9ad5075ad73f31a91c8cf3e0a67f5c6992f
NguyenPhucHoangAnh <113869027+NguyenPhucHoangAnh@users.noreply.github.com>
2024-03-04 19:53:48 +0700

    Add files via upload

---
+++ Lab05/Bai1/ThreadExample/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyThreadClass mtc1 = new MyThreadClass("Day la tieu trinh dau thu 1");
+            Thread thread1 = new Thread(new ThreadStart(mtc1.runMyThread));
+            thread1.Start();
+
+            MyThreadClass mtc2 = new MyThreadClass("Day la tieu trinh dau thu 2");
+            Thread thread2 = new Thread(new ThreadStart(mtc2.runMyThread));
+            thread2.Start();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab05/Bai2/Bai2/EchoProtocol.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace Bai2
+{
+    class EchoProtocol : IProtocol
+    {
+        public const int BUFSIZE = 32; //Byte size off IO buffer
+        private Socket clnSock; //Connection socket
+        private ILogger logger; //Logging Facility
+        public EchoProtocol(Socket clnSock , ILogger logger)
+        {
+            this.clnSock = clnSock;
+            this.logger = logger;
+        }
+
+        public void handleclient()
+        {
+            ArrayList entry =new ArrayList();
+            entry.Add("Client address and port = " + clnSock.RemoteEndPoint);
+            entry.Add("Thread = " + Thread.CurrentThread.GetHashCode());
+
+            try
+            {
+                int recvMsgSize;
+                int totalBytesEchoed = 0;
+                byte[] rcvBuffer = new byte[BUFSIZE];
+                try
+                {
+                    while ((recvMsgSize = clnSock.Receive(rcvBuffer, 0, rcvBuffer.Length, SocketFlags.None)) > 0)
+                    {
+                        clnSock.Send(rcvBuffer, 0, recvMsgSize, SocketFlags.None);
+                        totalBytesEchoed += recvMsgSize;
+                    }
+                }
+                catch (SocketException se)
+                {
+                    entry.Add(se.ErrorCode + ":" + se.Message);
+                }
+                entry.Add("Client finished; echoed " + totalBytesEchoed + " bytes.");
+            }
+            catch (SocketException se) 
+            {
+                entry.Add(se.ErrorCode + ":" + se.Message);
+            }
+            clnSock.Close();
+            logger.writeEntry(entry);
+        }
+    }
+}
